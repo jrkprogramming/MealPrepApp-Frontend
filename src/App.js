6f085c02ab4a5308b8961c1c2ac46e2548fedce3
jrkprogramming <jrkaibigan@gmail.com>
2022-10-13 18:01:12 -0500

    Set Up Google OAuth. Still need to implement it to be able to display and link users to recipes.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { gapi } from "gapi-script";
+import { useEffect } from "react";
+
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import RecipeIndexPage from "./Pages/IndexRecipePage/IndexRecipePage";
 import DetailsRecipePage from "./Pages/DetailsRecipePage/DetailsRecipePage";
 import NewRecipePage from "./Pages/NewRecipePage/NewRecipePage";
 
 function App() {
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId:
+          "8383221493-ekdghn5ufj7hghm92nkh2u6qh4op1ej1.apps.googleusercontent.com",
+        scope: "",
+      });
+    }
+    gapi.load("client:auth2", start);
+  });
+
   return (
     <div className="App">
       <Routes>
-        <Route element={LoginPage} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/mealPrep" element={<RecipeIndexPage />} />
         <Route path="/mealPrep/new" element={<NewRecipePage />} />
         <Route path="/mealPrep/:id" element={<DetailsRecipePage />} />
--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,36 +1,38 @@
 import React from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 
 const LoginPage = () => {
+  const onSuccessLogin = (res) => {
+    console.log("Login Successfull", res.profileObj);
+  };
+
+  const onFailureLogin = (res) => {
+    console.log("Logout failed", res);
+  };
+
+  const onSuccessLogout = () => {
+    console.log("Log Out Successfull");
+  };
+
   return (
-    <section class="vh-100" style="background-color: #508bfc;">
-      <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div class="card shadow-2-strong" style="border-radius: 1rem;">
-              <div class="card-body p-5 text-center">
-                <h3 class="mb-5">Welcome to Meal Prep App! Please Sign In</h3>
+    <section className="vh-100">
+      <div>
+        <GoogleLogin
+          clientId="8383221493-ekdghn5ufj7hghm92nkh2u6qh4op1ej1.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={onSuccessLogin}
+          onFailure={onFailureLogin}
+          cookiePolicy={"single_host_origin"}
+          isSignedIn={true}
+        />
+      </div>
 
-                <hr class="my-4"></hr>
-
-                <a
-                  href="/auth/google"
-                  class="btn btn-lg btn-block btn-primary"
-                  style="background-color: #dd4b39;"
-                  type="submit"
-                >
-                  <i class="fab fa-google me-2"></i>
-                  <img
-                    width="20px"
-                    style="margin-top:7px; margin-right:8px"
-                    alt="Google sign-in"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
-                  />{" "}
-                  Sign in with Google
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div>
+        <GoogleLogout
+          clientId="8383221493-ekdghn5ufj7hghm92nkh2u6qh4op1ej1.apps.googleusercontent.com"
+          buttonText="Logout"
+          onLogoutSuccess={onSuccessLogout}
+        />
       </div>
     </section>
   );
